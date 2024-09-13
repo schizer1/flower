8c97a250a7d809178670bd8767b57e4f9f16b439
schizer1 <dikkyjoss@gmail.com>
2024-09-14 00:13:18 +0700

    Add files via upload

---
+++ lirik.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new TypeIt(".lirik1", {
+    strings: ["mad at the world", "yeah i've been there before", "lost and confused", "and thinking that", "You can't take anymore", "no, no, no, no worry", "'about nothing, oh", "it's all gonna be good", "oh, oh, oh, oh", "You're really something, oh", "didn't You know that You could", "no, no, no, no worry", "'about nothing, oh", "it's all gonna be good", "worry 'bout nothing", "nothing, oh", "it's all gonna be good", "so breathe.....","like You know You should", "yeah,breathe....","till You've understood", "until You've feeling","like Yourself again", "feel the sunlight","on Your skin","keep Your heartbeat","beat-beating"],
+    speed: 10,
+  }).go();
+});
