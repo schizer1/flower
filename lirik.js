0677f87691b2aa91419d007929812d0ca90d4e6a
schizer1 <dikkyjoss@gmail.com>
2024-12-20 22:21:08 +0700

    Update lirik.js

--- lirik.js
+++ lirik.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   new TypeIt(".lirik1", {
-    strings: ["mad at the world", "yeah i've been there before", "lost and confused", "and thinking that", "You can't take anymore", "no, no, no, no worry", "'about nothing, oh", "it's all gonna be good", "oh, oh, oh, oh", "You're really something, oh", "didn't You know that You could", "no, no, no, no worry", "'about nothing, oh", "it's all gonna be good", "worry 'bout nothing", "nothing, oh", "it's all gonna be good", "so breathe.....","like You know You should", "yeah,breathe....","till You've understood", "until You've feeling","like Yourself again", "feel the sunlight","on Your skin","keep Your heartbeat","beat-beating"],
+    strings: ["Kau yang kucinta sepenuh hati", "Biarkan semua manusia", "Jadi saksi nyata", "Bahwa memilikimu adalah", "Anugerah terindah", "Untuk diriku...", "Selamanya...", "Tak akan tersakiti", "Kau yang kucinta sepenuh hati"],
     speed: 5,
   }).go();
 });
