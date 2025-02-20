242ba674f32bd9c4b9ece8da14ace7dd1a30a76e
schizer1 <dikkyjoss@gmail.com>
2025-02-21 02:46:31 +0700

    Add files via upload

---
+++ lirik.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new TypeIt(".lirik1", {
+    strings: ["Aku tau kamu hebat", "Namun selamanya diriku pasti berkutat", "Tuk slalu jauhkanmu dari dunia yang jahat", "Ini sumpahku padamu tuk biarkanmu", "Tumbuh lebih baik", "Cari panggilanmu jadi lebih baik", "Dibanding diriku", "Tuk sementara kita tertawakan", "Berbagai hal yang lucu & lara", "Selepas-lepasnya", "Saat dewasa kau kan mengerti"],
+    speed: 30,
+  }).go();
+});
